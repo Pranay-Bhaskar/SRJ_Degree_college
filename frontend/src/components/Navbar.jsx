402af158f06806c6c646e1bfda03ec05596dde31
Pranay B <b.pranay.git@gmail.com>
2026-04-27 11:39:23 +0530

    remove vi and mi

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -417,7 +417,6 @@ const navLinks = [
       { label: "About Us", path: "/about#about-college" },
       { label: "Principal's Message", path: "/about#principal-message" },
       { label: "Director's Message", path: "/about#director-message" },
-    {/*  { label: "Vision and Mission", path: "/about#vision-mission" }, */}
     ],
   },
   {
