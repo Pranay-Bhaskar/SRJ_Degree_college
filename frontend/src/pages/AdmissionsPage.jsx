681fcd610e09499ea70e596c7753a076ea89e4ea
Nishant9764 <151760559+Nishant9764@users.noreply.github.com>
2026-06-23 11:38:08 +0530

    Update AdmissionsPage.jsx

--- frontend/src/pages/AdmissionsPage.jsx
+++ frontend/src/pages/AdmissionsPage.jsx
@@ -135,7 +135,7 @@ export default function AdmissionsPage() {
             <div className="ad-hero__visual">
               <div className="ad-hero__imageWrap">
                 <img
-                  src="/images/campus1.jpg"
+                  src="/images/campus.jpg"
                   alt="Dr. S R J Degree College building"
                   className="ad-hero__image"
                 />
